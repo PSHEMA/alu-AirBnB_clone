88cffd3e3faa981f31ac298ad6a6b2e374d719d3
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-09-07 15:55:14 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,40 @@
 #!/usr/bin/python3
-""" Module for Base """
+"""base model"""
 
+import uuid
 from datetime import datetime
-from uuid import uuid4
-import json
-import models
 from models import storage
 
-format_date = "%Y-%m-%dT%H:%M:%S.%f"
-
-
 class BaseModel:
-    """ Basemodel class """
+    """BaseModel class"""
     def __init__(self, *args, **kwargs):
-        """ Initialization of Database """
-        if args is not None and len(args) > 0:
-            pass
         if kwargs:
-            for key, item in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    item = datetime.strptime(item, format_date)
-                if key not in ['__class__']:
-                    setattr(self, key, item)
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        # Convert string to datetime object
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
         else:
-            self.id = str(uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)  # Add the object to the storage
+             self.id = str(uuid.uuid4())
+             self.created_at = datetime.now()
+             self.updated_at = datetime.now()
+             storage.new(self)
 
     def __str__(self):
-        """str definition"""
+        """str representation"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """save definition"""
+        """update the updated_at with the current time"""
         self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        """returns a dictionary representation of the object"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
