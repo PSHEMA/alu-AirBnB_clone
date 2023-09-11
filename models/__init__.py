c5884d241b8612c39fa8e96399bdce433e9f455f
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-09-11 14:09:30 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """init file for models"""
 
+from model.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
+classes = {}
