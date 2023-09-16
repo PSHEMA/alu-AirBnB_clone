2e7cfcdf46bed31d79f45aaa0548cef7a133137c
Elvin Mgeni <116630928+Mgeni-E@users.noreply.github.com>
2023-09-16 13:42:45 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python3
-"""base model"""
+"""The base model class which is going to act as a parent"""
 
-import uuid
+import models
 from datetime import datetime
-from models import storage
+from uuid import uuid4
+
 
 class BaseModel:
-    """BaseModel class"""
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        # Convert string to datetime object
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                    else:
-                        setattr(self, key, value)
-        else:
-             self.id = str(uuid.uuid4())
-             self.created_at = datetime.now()
-             self.updated_at = datetime.now()
-             storage.new(self)
+    """Start of the Base model class"""
 
-    def __str__(self):
-        """str representation"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    def __init__(self, *args, **kwargs):
+        """Base model constructor taking args and kwargs"""
+        timeform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    self.__dict__[k] = datetime.strptime(v, timeform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
-        """update the updated_at with the current time"""
-        self.updated_at = datetime.now()
-        storage.save()
+        """updates the UPDATED_AT instance"""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary representation of the object"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """Returns dictionary representation of the instance"""
+        dictionary = self.__dict__.copy()
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary["__class__"] = self.__class__.__name__
+        return dictionary
+
+    def __str__(self):
+        """Returns the string representation"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
